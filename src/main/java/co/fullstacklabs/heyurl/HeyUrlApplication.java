98bfa0d7fadd00ea34402601d8e451a7de080382
Sam Hanes <sam@fullstacklabs.co>
2020-01-13 14:42:42 -0800

    Start with Spring Initializr.

---
+++ src/main/java/co/fullstacklabs/heyurl/HeyUrlApplication.java
@@ -0,0 +1,13 @@
+package co.fullstacklabs.heyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/fullstacklabs/heyurl/HeyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package co.fullstacklabs.heyurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
